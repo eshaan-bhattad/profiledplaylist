43ad1c0aeb3cb9c3ada4695516cf74219264b8ce
briancy02 <byoon2002@gmail.com>
2021-01-10 01:49:28 +0200

    before push

--- profileplaylist/core/views.py
+++ profileplaylist/core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from spotify.views import IsAuthenticated
 # Create your views here.
+
+
 
 def home(request):
 	testing = "Profiled Playlist"
+	testing = IsAuthenticated.get(request, None)
 	return render(request, 'index.html', {
 		'testing' : testing,
 		})
